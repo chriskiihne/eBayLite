836e46272657e3d631bd470dab9ca84710b8a481
cbrammeier7538 <cbrammeier@unomaha.edu>
2021-11-16 23:50:15 -0600

    Updated Homepage to show items.
    Updated login/register/auctioning to point to correct homepage.

--- eBayLite/eBayLite/src/Homepage.java
+++ eBayLite/eBayLite/src/Homepage.java
@@ -1,11 +1,21 @@
-
-
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import util.DBConnection;
 
 /**
  * Servlet implementation class Homepage
@@ -27,9 +37,101 @@ public class Homepage extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		display(request, response);
 	}
-
+	void display(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		out.println("<!DOCTYPE html>\n" + 
+				"<html>\n" + 
+				"<head>\n" + 
+				"<meta charset=\"ISO-8859-1\">\n" + 
+				"<style>\n" + 
+				"header {\n" + 
+				"	background-color: green;\n" + 
+				"	color: white;\n" + 
+				"	text-align: center;\n" + 
+				"	padding: 5px;\n" + 
+				"}\n" + 
+				"nav {\n" + 
+				"	line-height: 30px;\n" + 
+				"	background-color: #eeeeee;\n" + 
+				"	height: 300px;\n" + 
+				"	width: 100px;\n" + 
+				"	float: Left;\n" + 
+				"	padding: 5px;\n" + 
+				"}\n" + 
+				"a {\n" + 
+				"	display: block;\n" + 
+				"}\n" + 
+				"section {\n" + 
+				"	width: 350px;\n" + 
+				"	float: Left;\n" + 
+				"	padding: 10px;\n" +
+				"}\n" + 
+				"footer {\n" + 
+				"	background-color: green;\n" + 
+				"	color: white;\n" + 
+				"	clear: both;\n" + 
+				"	text-align: center;\n" + 
+				"	padding: 5px;\n" + 
+				"}\n" + 
+				"</style>\n" + 
+				"<title>Ebay Lite</title>\n" + 
+				"</head>\n" + 
+				"	<body>\n" + 
+				"		<header>\n" + 
+				"			<h1> Ebay Lite Home </h1>\n" + 
+				"		</header>\n" + 
+				"		<nav>\n" + 
+				"			<a href=\"/eBayLite/Login.html\">Login</a>\n" + 
+				"			<a href=\"/eBayLite/Auctioning.html\">Auctioning</a>\n" + 
+				"		</nav>\n" + 
+				"		<section>\n" + 
+				"			<h1> Items </h1>\n" + 
+				"			<p> If you are interested please search for an item below. </p>");
+		try {
+			DBConnection.getConnection();
+			connection = DBConnection.getConnection();
+			String selectSQL = "SELECT * FROM items";
+			preparedStatement = connection.prepareStatement(selectSQL);
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String auctionStart = rs.getString("auctionstart");
+				String auctionEnd = rs.getString("auctionend");
+				out.println("<a href=\"ItemView.html?item=" + id + "\" id=\"" + id + "\">" + name + " " + auctionStart + " " + auctionEnd + "</a>");
+			}
+			rs.close();
+			preparedStatement.close();
+			connection.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (SQLException se2) {
+		}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		out.println("</section>\n" + 
+				"		<footer>\n" + 
+				"		Copyright\n" + 
+				"		</footer>\n" + 
+				"		</body>\n" + 
+				"		</html>");
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -39,3 +141,4 @@ public class Homepage extends HttpServlet {
 	}
 
 }
+
